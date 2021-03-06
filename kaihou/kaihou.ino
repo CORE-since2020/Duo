359394a14d3af7e1bf5ab374097a618a2c73c608
Rio Kawate <riokawate.telstar@gmail.com>
2021-03-06 12:06:30 +0900

    Update kaihou.ino

--- kaihou/kaihou.ino
+++ kaihou/kaihou.ino
@@ -1,4 +1,4 @@
-
+int i=0;
 void setup()
 {
   double p[]; //気圧センサから取得した気圧値を格納する
@@ -10,26 +10,23 @@ void setup()
 
 void loop()
 {
-//気圧センサから取得した気圧値の5回移動平均が１つ前の値より大きいことを10回連続で確認する．（最高点に到達し高度が下がっていることを確認する）
-    p_ave1 = (p[i]+p[i+1]+p[i+2]+p[i+3]+p[i+4]) / 5;
-    p_ave2 = (p[i+1]+p[i+2]+p[I+3]+p{i+4]+p[i+5])/ 5;
+    while(i < 10)
+    {  
+          p_ave1 = (p[i]+p[i+1]+p[i+2]+p[i+3]+p[i+4]) / 5;
+          p_ave2 = (p[i+1]+p[i+2]+p[I+3]+p{i+4]+p[i+5])/ 5;
     
-    t_ave1 = (t[i]+t[i+1]+t[i+2]+t[i+3]+t[i+4]) / 5;
-    t_ave2 = (t[i+1]+t[i+2]+t[I+3]+t{i+4]+t[i+5])/ 5;
+          t_ave1 = (t[i]+t[i+1]+t[i+2]+t[i+3]+t[i+4]) / 5;
+          t_ave2 = (t[i+1]+t[i+2]+t[I+3]+t{i+4]+t[i+5])/ 5;
    
-    h_old = (pow(1013.25/p_ave1, 1/5.257) - 1)*(t_ave1+273.15) / 0.0065;
-    h_new = (pow(1013.25/p_ave2, 1/5.257) - 1)*(t_ave2+273.15) / 0.0065;
+          h_old = (pow(1013.25/p_ave1, 1/5.257) - 1)*(t_ave1+273.15) / 0.0065;
+          h_new = (pow(1013.25/p_ave2, 1/5.257) - 1)*(t_ave2+273.15) / 0.0065;
 
-    if(p_ave1 < p_ave2){
-      printf("p_ave2 higher\n");
-    }
-    else if(p_ave1 > p_ave2){
-      printf("p_ave2 lower\n");
-    }
-    else if(h_old < h_new){
-      printf("h_new higher\n");
-    }
-    else{
-      printf("h_new lower\n");
-    }
+          if(h_old < h_new){
+             Serial.println(h_new higher);
+          }
+          else{
+             Serial.println(h_new lower);
+          }
+          i++;
+     }
  }

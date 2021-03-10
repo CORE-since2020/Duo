e1470795d29a154751f846f1d5fdaa213a92b145
Rio Kawate <riokawate.telstar@gmail.com>
2021-03-10 09:57:42 +0900

    Update kaihou.ino

--- kaihou/kaihou.ino
+++ kaihou/kaihou.ino
@@ -13,10 +13,10 @@ void loop()
     while(i < 10)
     {  
           p_ave1 = (p[i]+p[i+1]+p[i+2]+p[i+3]+p[i+4]) / 5;
-          p_ave2 = (p[i+1]+p[i+2]+p[I+3]+p{i+4]+p[i+5])/ 5;
+          p_ave2 = (p[i+1]+p[i+2]+p[i+3]+p[i+4]+p[i+5])/ 5;
     
           t_ave1 = (t[i]+t[i+1]+t[i+2]+t[i+3]+t[i+4]) / 5;
-          t_ave2 = (t[i+1]+t[i+2]+t[I+3]+t{i+4]+t[i+5])/ 5;
+          t_ave2 = (t[i+1]+t[i+2]+t[i+3]+t[i+4]+t[i+5])/ 5;
    
           h_old = (pow(1013.25/p_ave1, 1/5.257) - 1)*(t_ave1+273.15) / 0.0065;
           h_new = (pow(1013.25/p_ave2, 1/5.257) - 1)*(t_ave2+273.15) / 0.0065;
@@ -26,7 +26,7 @@ void loop()
           }
           else{
              Serial.println(h_new lower);
+             i++
           }
-          i++;
      }
  }

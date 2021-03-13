3f64db1978509f22eaaad740e162abdacf01d253
DDD3H <40761694+DDD3H@users.noreply.github.com>
2021-03-13 17:55:59 +0900

    Update DifferentialpressureSensor_code_binver.ino

--- DifferentialpressureSensor_code_binver.ino
+++ DifferentialpressureSensor_code_binver.ino
@@ -6,7 +6,7 @@ int sensorPin = A0;
 int sensorValue = 0;
 int n = 1;
 int count;
-int SAMPLING_RATE = 50;
+int SAMPLING_RATE = 20000;
 const char* file_name = "/test.bin";
 File myFile;
 
@@ -25,45 +25,46 @@ void IRAM_ATTR onTimer1(){
 
 
 void setup() {
-	SD.begin(5);
-	timer1 = timerBegin(0, 80, true);
-	timerAttachInterrupt(timer1, &onTimer1, true);
-	timerAlarmWrite(timer1, 1.0E6 / SAMPLING_RATE, true);
-	timerAlarmEnable(timer1);
-	
-	Serial.begin(115200);
-	
-	myFile = SD.open(file_name, FILE_APPEND);
+  SD.begin(5);
+  timer1 = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer1, &onTimer1, true);
+  timerAlarmWrite(timer1, 1.0E6 / SAMPLING_RATE, true);
+  timerAlarmEnable(timer1);
 
-  		while (!SD.begin(5)) {
-			Serial.println("ERROR");
-		}
+  Serial.begin(115200);
+
+  myFile = SD.open(file_name, FILE_APPEND);
+
+      while (!SD.begin(5)) {
+      Serial.println("ERROR");
+    }
 }
 
 void loop() {
-	if (timeCounter1 > 0) {
-		portENTER_CRITICAL(&timerMux);
-		timeCounter1--;
-		portEXIT_CRITICAL(&timerMux);
-		
-		myFile = SD.open(file_name, FILE_APPEND);
-		sensorValue = analogRead(sensorPin);
-		
-		byte buf[4];
-		casttobyte(sensorValue,buf);
-		myFile.write(buf,sizeof(buf));
-		myFile.close();
-		
-		Serial.print("Done");
-		Serial.println(n);
-		++n;
+  if (timeCounter1 > 0) {
+    portENTER_CRITICAL(&timerMux);
+    timeCounter1--;
+    portEXIT_CRITICAL(&timerMux);
+
+    sensorValue = analogRead(sensorPin);
+
+    byte buf[1];
+    casttobyte(sensorValue,buf);
+    myFile.write(buf,sizeof(buf));
+
+
+    if((n%20000) == 0){
+      Serial.print("Done");
+      Serial.println(n);
+      myFile.close();
+      myFile = SD.open(file_name, FILE_APPEND);  
+    }
+
+      ++n;
 }
 }
 
-
-void casttobyte(int32_t data, byte buf[]){
-	buf[0] = (data >> 24) & 0x00FF;
-	buf[1] = (data >> 16) & 0x00FF;
-	buf[2] = (data >> 8) & 0x00FF;
-	buf[3] = (data) & 0x00FF;
+void casttobyte(int16_t data, byte buf[]){
+  buf[0] = (data >> 8) & 0x00FF;
+  buf[1] = (data) & 0x00FF;
 }

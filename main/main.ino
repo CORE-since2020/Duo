0d196b608ffbe6c45655b35d434d08dbe3a212c5
吉田健人 <52515974+4456-kent@users.noreply.github.com>
2021-03-16 17:05:04 +0900

    Create main
    離床判定まで

---
+++ main/main.ino
@@ -0,0 +1,232 @@
+/*インクルード*/
+#include "mpu6050.h"
+#include "bme280.h"
+
+/*サンプリングレート*/
+const double sampling_rate = 200.0;
+
+/*離床判定の閾値[G]*/
+const int judge_acc = 2.5;
+/*離床判定のカウント数*/
+const int LAUNCH_COUNT = 5;
+
+/*ピンアサイン*/
+const int wireless_rx = 16;
+const int wireless_tx = 17;
+const int sda = 21;
+const int scl = 22;
+
+/*その他の定数*/
+const int acc_range_mode = 2;
+const int gyro_range_mode = 3;
+
+/*タイマー割込み処理関係の変数*/
+volatile int timeCounter1;
+hw_timer_t *timer1 = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+const double standby_rate = 10;
+
+/*関数のプロトタイプ宣言*/
+void standby();
+void launch();
+void IRAM_ATTR onTimer1();
+void LoraInit();
+void clearBuffer();
+double calc_altitude(double, double);
+double average(double*, int);
+
+/*フェーズの定義*/
+typedef enum phase{
+  STANDBY,
+  LAUNCH,
+  BURNING,
+  RISIN,
+  PARACHUTE
+} phase;
+
+phase p = STANDBY;  //フェーズの初期化
+
+/*初期化処理かループ処理か分ける変数の定義*/
+typedef enum ProsessingMode{
+  INIT,
+  LOOP
+} ProcessingMode;
+
+ProcessingMode pm = INIT;  //処理の種類識別の変数宣言
+
+/*その他の変数*/
+double s_press = 1006.20; //オフセット用の気圧
+double acc[5]; //離床判定用の加速度格納配列
+int j = 0;  //配列ナンバリング(加速度)
+int L_judgeCounter = 0; //離床判定のカウンター
+unsigned long t_zero; //離床判定確定時の時間
+
+/*オブジェクト宣言*/
+mpu6050 mpu(sda, scl, acc_range_mode, gyro_range_mode);
+bme280 bme(sda, scl);
+
+void setup() {
+  /*MPU6050の初期化*/
+  mpu.setup();
+  
+  /*BME280の初期化*/
+  bme.setup();
+   
+  /*ES920LRの初期化*/
+  Serial2.begin(115200, SERIAL_8N1, wireless_rx, wireless_tx); //無線機-ESP32
+  LoraInit();
+  Serial2.print("Hello! Hibari1\r\n");
+
+}
+
+void loop() {
+  switch(p){
+    case STANDBY:
+      /*スタンバイフェーズの処理*/
+      standby();
+      break;
+      
+    case LAUNCH:
+      /*ローンチフェーズの処理*/
+      launch();
+      break;
+      
+    default:
+      /*スタンバイフェーズに戻る*/
+      p = STANDBY;
+      break;
+  }
+}
+
+/*スタンバイフェーズ処理関数*/
+void standby(){
+  char buf[30] = "";
+  unsigned char i = 0;
+  while(Serial2.available() > 0){
+    buf[i] = Serial2.read();
+
+    if(buf[i] == '\r' || buf[i] == '\n'){
+      buf[i] = '\0';
+      clearBuffer();
+      break;
+    }
+    if(i < 30){
+      i++;
+    }else{
+      i = 0;
+    }
+  }
+  if(strcmp(buf, "flight") == 0){
+    p = LAUNCH;
+    Serial2.print("FLIGHT_MODE!\r\n");
+  }else if(strcmp(buf, "info") == 0){
+    int16_t a[3];
+    double b[3];
+    mpu.getValueRawACC(a);
+    double syn_acc = mpu.SynthesizeACC(a[0],a[1],a[2]);
+
+    bme.getValue(b);
+    double alt = calc_altitude(b[0],b[1],s_press);
+
+    Serial2.print("STANDBY "); Serial2.print("ACC:");  Serial2.print(syn_acc); Serial2.print("[G] ");
+    Serial2.print("ALT:");  Serial2.print(alt); Serial2.print("[m]\r\n");
+  }
+}
+
+void launch(){
+  switch(pm){
+    case INIT:{
+      /*タイマー割込み処理開始*/ 
+      timer1 = timerBegin(0, 80, true);
+      timerAttachInterrupt(timer1, &onTimer1, true);
+      timerAlarmWrite(timer1, 1.0E6 / sampling_rate, true);
+      timerAlarmEnable(timer1);  
+
+      pm = LOOP;  //処理の種類をLOOP変更
+    }break;
+    case LOOP:{
+      interrupts();
+      if(timeCounter1 > 0){
+        portENTER_CRITICAL(&timerMux);
+        timeCounter1--;
+        portEXIT_CRITICAL(&timerMux);
+        /*割込み処理*/
+        int16_t data[3];
+        mpu.getValueRawACC(data);
+        acc[j] = mpu.SynthesizeACC(data[0], data[1], data[2]);
+        j++;
+        if(j > 4){
+          j = 0;
+        }
+      }
+      noInterrupts();
+      /*離床判定*/
+      if(average(acc, 5) >= judge_acc){
+        L_judgeCounter++;
+      }else{
+        L_judgeCounter = 0;
+      }
+      if(L_judgeCounter > LAUNCH_COUNT){
+        /*離床を確認*/
+        timerEnd(timer1);
+        t_zero = millis();
+        p = BURNING;
+        pm = INIT;
+        Serial2.print("LAUNCH!\r\n");
+      }
+    }break;
+    default:{
+      /*INITに戻る*/
+      pm = INIT;
+    }break;
+  }
+}
+
+void IRAM_ATTR onTimer1(){
+  portENTER_CRITICAL_ISR(&timerMux);
+  timeCounter1++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+void LoraInit(){
+  // コマンドモード開始
+  Serial2.print("2\r\n"); clearBuffer();
+  // 子機(End Device)に設定
+  Serial2.print("node 2\r\n"); clearBuffer();
+  // 自分のノードIDを設定
+  Serial2.print("ownid 0001\r\n"); clearBuffer();
+  //送信先のノードID
+  Serial2.print("dstid 0000\r\n"); clearBuffer();
+  // 起動時configuration
+  Serial2.print("q 1\r\n");
+  //保存
+  Serial2.print("w\r\n"); clearBuffer();
+  // 通信の開始
+  Serial2.print("z\r\n"); clearBuffer();
+}
+
+void clearBuffer(){
+  delay(100);
+  while (Serial2.available() > 0) Serial2.read();
+}
+
+double calc_altitude(double pres, double temp, double offset_p){
+  double alt;
+
+  alt = (pow(offset_p /pres, 1 / 5.257) - 1) * (temp + 273.15) / 0.0065;
+
+  return alt;
+}
+
+double average(double* e, int len){
+  double sum = 0;
+  double ave;
+  for(int i = 0; i < len; i++){
+    sum += e[i];
+  }
+
+  ave = sum / len;
+
+  return ave;
+}
